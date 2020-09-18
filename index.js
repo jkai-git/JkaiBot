e0ac42a5f0c1df3880ffce2c507e9952936b5a5a
jkai-git <germanjakab2@gmail.com>
2020-09-18 18:18:37 +0200

    basic client setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const { prefix, token } = require('./config.json');
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	console.log(message.content);
+
+	if (message.content.startsWith(`${prefix}ping`))
+		message.channel.send('Pong.');
+	else if (message.content(`${prefix}beep`))
+		message.channel.send('Boop.');
+});
+
+client.login(token);

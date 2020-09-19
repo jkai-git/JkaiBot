2642a288e4e48148cd5048dce596d8157702f5bf
jkai-git <germanjakab2@gmail.com>
2020-09-19 09:44:16 +0200

    .startsWith

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ client.on('message', message => {
 
 	if (message.content.startsWith(`${prefix}ping`))
 		message.channel.send('Pong.');
-	else if (message.content(`${prefix}beep`))
+	else if (message.content.startsWith(`${prefix}beep`))
 		message.channel.send('Boop.');
 });
 

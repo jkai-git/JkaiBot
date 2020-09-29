e19bd8f3a0e1fd87a8e3c06530dadfb5966cf497
jkai-git <germanjakab2@gmail.com>
2020-09-29 15:59:21 +0200

    avatar command accepts @everyone

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,6 +2,15 @@ module.exports = {
 	name: 'avatar',
 	description: 'Avatar lookup of mentioned users.',
 	execute(message, args) {
+		if (message.mentions.everyone) {
+			message.guild.members.fetch().then(memberCollection => {
+				const avatarList = memberCollection.map(member => {
+					return `${member.user.username}'s avatar: ${member.user.displayAvatarURL({ dynamic: true, size: 4096 })}`;
+				});
+				message.reply(avatarList);
+			});
+			return;
+		}
 		if (!message.mentions.users.size) {
 			return message.channel.send(`Your avatar: ${message.author.displayAvatarURL({ dynamic: true, size: 4096 })}`);
 		}

44bff63086828744895d964cea851f63edc9cd6f
jkai-git <germanjakab2@gmail.com>
2021-04-21 23:04:06 +0200

    keyv, postgresql setup

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const { token } = require('./token.json');
-
-const fs = require('fs');
+const { token, dbConnectString } = require('./connect.json');
 const Discord = require('discord.js');
+const Keyv = require('keyv');
+const Fs = require('fs');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 client.cooldowns = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const keyv = new Keyv(dbConnectString);
+keyv.on('error', err => console.error('Keyv connection error:', err));
+
+const commandFiles = Fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
 }
 
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const eventFiles = Fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 for (const file of eventFiles) {
 	const event = require(`./events/${file}`);
 	if (event.once) {

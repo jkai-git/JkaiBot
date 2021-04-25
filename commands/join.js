fb6485e6a798e2d6eb31622150522efc5a37c687
jkai-git <germanjakab2@gmail.com>
2021-04-25 10:10:32 +0200

    rem

--- commands/join.js
+++ commands/join.js
@@ -6,9 +6,6 @@ module.exports = {
 	execute(message, args) {
 		if (!message.member.voice.channel) return message.channel.send('You are not in a voice channel.');
 		if (!message.member.voice.channel.joinable) return message.channel.send('I can\'t join the voice channel that you are in.');
-		if (connections.has(message.guild.id) && connections.get(message.guild.id).channel.id === message.member.voice.channel.id) {
-			return message.channel.send('I\'m already in this channel.');
-		}
 		return message.member.voice.channel.join()
 			.then(connection => {
 				connection.voice.setSelfDeaf(true);

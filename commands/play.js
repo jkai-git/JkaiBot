13adcf204a6fb27aad5437d520085abdb3a4c087
jkai-git <germanjakab2@gmail.com>
2021-04-25 12:17:38 +0200

    ytdl-core-discord added

--- commands/play.js
+++ commands/play.js
@@ -8,7 +8,27 @@ module.exports = {
 	cooldown: 3,
 	async execute(message, args) {
 		if (!connections.has(message.guild.id)) await commands.get('join').execute(message, args);
+		if (!connections.has(message.guild.id)) return;
+		const connection = connections.get(message.guild.id);
+
+		connection.play(await ytdl(/*url*/, ytdlOptions('2m')), dispatcherOptions);
 	}
 };
 
 const { connections, commands } = require('../global.js');
+const ytdl = require('ytdl-core-discord');
+
+const ytdlOptions = seek => {
+	return {
+		begin: seek,
+		quality: 'highestaudio',
+		filter: 'audioonly'
+	};
+}
+
+const dispatcherOptions = {
+	type: 'opus',
+	bitrate: 'auto',
+	highWaterMark: 50,
+	volume: false
+};
